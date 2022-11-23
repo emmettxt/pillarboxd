d994de88db2e8a78c4f2bad73f28e19fbdee10a1
Emmet Moore <x21192090@student.ncirl.ie>
2022-11-23 14:51:28 +0100

    Updated user shows page to show more details

--- frontend/src/components/UserShowsPage.js
+++ frontend/src/components/UserShowsPage.js
@@ -1,24 +1,122 @@
-import {useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Container } from '@mui/system'
-import { Link, Card, CardContent, CardMedia, Typography } from '@mui/material'
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActionArea,
+  Divider,
+  CardActions,
+  Box,
+} from '@mui/material'
+import tvService from '../services/tv'
+import { useEffect, useState } from 'react'
+import userShowsService from '../services/userShows'
+import { setShow } from '../reducers/userReducer'
+import WatchedIconButton from './WatchedIconButton'
+const formatEpisodeDate = (dateAsString) => {
+  const date = new Date(dateAsString)
+  if (!dateAsString || date.toString === 'Invalid Date') {
+    return null
+  }
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  return date.toLocaleDateString(undefined, options)
+}
 const ShowCard = ({ tv }) => {
+  const getNextEpisode = async () => {
+    const season = tv.tmdb.seasons
+      .filter((season) => season.season_number !== 0)
+      .sort((a, b) => a.season_number - b.season_number)
+      .find(
+        (season) =>
+          season.episode_count >
+          tv.episodes.filter((x) => x.season_number === season.season_number)
+            .length
+      )
+    if (!season) return null
+    const episode_number = Array.from(
+      Array(season?.episode_count).keys(),
+      (i) => i + 1
+    ).find(
+      (episode_number) =>
+        tv.episodes.filter(
+          (x) =>
+            x.season_number === season.season_number &&
+            x.episode_number === episode_number
+        ).length === 0
+    )
+    const episode = tvService.getEpisode(
+      tv.tmdb.id,
+      season.season_number,
+      episode_number
+    )
+    return episode
+  }
+  const [nextEpisode, setNextEpisode] = useState()
+  useEffect(() => {
+    getNextEpisode().then((e) => setNextEpisode(e))
+  }, [tv])
+  const dispatch = useDispatch()
+  const user = useSelector((state) => state.user)
+  const handleAddEpisode = async () => {
+    const updatedShow = await userShowsService.addEpisode(
+      user,
+      tv.tmdb.id,
+      nextEpisode.season_number,
+      nextEpisode.episode_number
+    )
+    dispatch(setShow({ tvId: tv.tmdb.id, updatedShow }))
+  }
   return (
-    <Card>
+    // >
+    <Card
+      sx={{ height: '100%', display: 'grid', gridTemplateColumns: '1fr 2fr' }}
+    >
+      <CardActionArea href={`/tv/${tv.tmdb.id}`}>
+        <CardMedia
+          component="img"
+          image={`https://image.tmdb.org/t/p/w185/${tv.tmdb.poster_path}`}
+          alt={tv.name + 'poster'}
+        />
+      </CardActionArea>
       <CardContent>
-        <Link
-          // <Link
-          href={`/tv/${tv.id}`}
-        >
-          <CardMedia
-            component="img"
-            // height="140"
-            image={`https://image.tmdb.org/t/p/w185/${tv.poster_path}`}
-            alt={tv.name + 'poster'}
-          />
-          <Typography textAlign="center">{tv.name}</Typography>
-        </Link>
+        <CardActionArea href={`/tv/${tv.tmdb.id}`}>
+          <Typography variant="h6" textAlign="center">
+            {tv.tmdb.name}
+          </Typography>
+        </CardActionArea>
+
+        <Divider />
+        {nextEpisode ? (
+          <Box>
+            <Typography varient="body2" color={'text.secondary'}>
+              {`Next Episode: S:${nextEpisode.season_number} E:${nextEpisode.episode_number} -  ${nextEpisode.name}`}
+            </Typography>
+            <Typography varient="body2" color={'text.secondary'}>
+              {new Date(nextEpisode.air_date) > new Date()
+                ? formatEpisodeDate(nextEpisode.air_date)
+                : null}
+            </Typography>
+
+            <WatchedIconButton
+              handleClick={handleAddEpisode}
+              isInWatchlist={false}
+            />
+          </Box>
+        ) : (
+          <Typography varient="body2" color={'text.secondary'}>
+            All Episodes Watched
+          </Typography>
+        )}
       </CardContent>
     </Card>
+    // </Link>
   )
 }
 
@@ -27,23 +125,21 @@ const UserShowsPage = () => {
   const shows = user?.shows
     ? Object.entries(user.shows)
         .filter((a) => a[1].isSaved)
-        .map((a) => a[1].tmdb)
+        .map((a) => a[1])
     : []
   return (
     <Container
-      maxWidth="md"
       sx={{
         display: 'grid',
         gridTemplateColumns: {
-          xs: 'repeat(3,1fr)',
-          sm: 'repeat(4,1fr)',
-          md: 'repeat(5,1fr)',
-
+          xs: 'repeat(1,1fr)',
+          sm: 'repeat(2,1fr)',
+          md: 'repeat(3,1fr)',
         },
       }}
     >
       {shows.map((tv) => (
-        <ShowCard tv={tv} key={tv.id} />
+        <ShowCard tv={tv} key={tv.tmdb.id} />
       ))}
     </Container>
   )
