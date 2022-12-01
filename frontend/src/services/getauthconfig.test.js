6a2248881e7aa7cc1a1571327aa943af2bf0d23c
Emmet Moore <x21192090@student.ncirl.ie>
2022-12-01 21:02:31 +0100

    clean up

--- frontend/src/services/getauthconfig.test.js
+++
@@ -1,7 +0,0 @@
-import { getAuthConfig } from './watchlist'
-
-test('with user', () => {
-  const user = { token: 'blahbalabla' }
-  const returned = getAuthConfig(user)
-  console.log(returned)
-})
