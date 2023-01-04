01091bc3e998842795e5597003f752260f9cca5e
Emmet Moore <x21192090@student.ncirl.ie>
2023-01-04 10:06:39 +0100

    fixed test

--- backend/Controllers/tmdbProxy.test.js
+++ backend/Controllers/tmdbProxy.test.js
@@ -1,4 +1,4 @@
-const app = require('../app')
+const app = require('../app/app')
 const supertest = require('supertest')
 const api = supertest(app)
 const axios = require('axios')
@@ -8,6 +8,6 @@ test('TV search works', async () => {
   const directResponse = await axios.get(
     `https://api.themoviedb.org/3/search/tv?api_key=${api_key}&${query}`
   )
-  const apiResponse = await api.get(`/tmdb/search/tv?${query}`).expect(200)
+  const apiResponse = await api.get(`/api/tmdb/search/tv?${query}`).expect(200)
   expect(apiResponse.body).toEqual(directResponse.data)
 })
